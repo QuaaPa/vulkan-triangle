6a0660d419bf68d274518be2526ea5aea13cad5d
QuaaPa <vadimkolno@gmail.com>
2026-01-30 13:44:15 +0100

    Shader modules

---
+++ src/pch.hh
@@ -0,0 +1,17 @@
+#ifndef PCH_H
+#define PCH_H
+
+// .std
+#include <iostream>
+#include <stdexcept>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <cstdint>
+#include <limits>
+#include <optional>
+#include <set>
+#include <fstream>
+
+#endif // PCH_H
